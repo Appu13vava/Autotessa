4b1757b9d694072b7ce82ef638dfec60dad5f7f4
Appu13vava <94307842+Appu13vava@users.noreply.github.com>
2025-05-18 07:04:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -66,10 +66,7 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
 ## EXTRA FEATURES ##
 
-      # URL Shortener #
-USE_SHORTLINK = is_enabled(environ.get("USE_SHORTLINK", "False"), False)
-URL_SHORTENR_WEBSITE = environ.get('')
-URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '')
+
 
      # Auto Delete For Group Message (Self Delete) #
 SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 40))
