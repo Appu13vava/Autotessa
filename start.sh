153b502d22bb127479f4b97e0ef36b868f9110f1
Appu13vava <94307842+Appu13vava@users.noreply.github.com>
2025-05-18 05:57:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,23 @@
-if [ -z $UPSTREAM_REPO ]
+#!/bin/bash
+
+# Clone repo
+if [ -z "$UPSTREAM_REPO" ]
 then
   echo "Cloning main Repository"
   git clone https://github.com/AM-ROBOTS/AutoAnura /AutoAnura
 else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  echo "Cloning Custom Repo from $UPSTREAM_REPO"
   git clone $UPSTREAM_REPO /AutoAnura
 fi
+
 cd /AutoAnura
+
+# Install requirements
 pip3 install -U -r requirements.txt
+
+# Start a dummy HTTP server for Koyeb health check (on port 8080)
+python3 -m http.server 8080 &
+
+# Start your bot
 echo "Starting Bot...."
 python3 bot.py
